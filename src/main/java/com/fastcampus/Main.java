ddc1a1d55fa72a8b2ec4603db1eea99671ab8a58
ehstar <ehwnsdh@naver.fcom>
2024-05-11 21:11:07 +0900

    gitflow-feature-stash: test2

--- src/main/java/com/fastcampus/Main.java
+++ src/main/java/com/fastcampus/Main.java
@@ -1,13 +1,13 @@
 package com.fastcampus;
 
+import com.fastcampus.logic.Sort;
+
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        JavaSortTest<String> javaSort = new JavaSortTest<>();
-        System.out.println(javaSort.sort(List.of("3", "2", "5", "4", "1")));
+        Sort<String> sort = new JavaSortTest<>();
 
-        System.out.println("Hello world!");
     }
 }
