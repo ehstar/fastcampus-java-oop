68a3cc4e912320a29bcce3b1832cb2fefbb3c8ea
ehstar <ehwnsdh@naver.fcom>
2024-05-11 21:25:47 +0900

    #3 강의테스트
    #3 강의테스트

--- src/main/java/com/fastcampus/Main.java
+++ src/main/java/com/fastcampus/Main.java
@@ -11,6 +11,9 @@ public class Main {
 
         System.out.println(sort.sort(List.of("3", "2", "5", "4", "1")));
 
+        Sort<String> sort2 = new BubbleSort<>();
+
+        System.out.println(sort2.sort(List.of("3", "2", "5", "4", "1")));
 
     }
 }
