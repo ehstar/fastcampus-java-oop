d8ac10ba55a05a06ca2cb4e67c3ab3b8a22141ca
ehstar <ehwnsdh@naver.fcom>
2024-05-11 20:50:43 +0900

    강의 실습 테스트
    강의 실습 테스트

---
+++ src/main/java/com/fastcampus/JavaSortTest.java
@@ -0,0 +1,15 @@
+package com.fastcampus;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class JavaSortTest <T extends Comparable<T>> {
+
+    public List<T> sort(List<T> list) {
+        List<T> output = new ArrayList<>(list);
+        Collections.sort(output);
+
+        return output;
+    }
+}
--- src/main/java/com/fastcampus/Main.java
+++ src/main/java/com/fastcampus/Main.java
@@ -1,7 +1,13 @@
 package com.fastcampus;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        JavaSortTest<String> javaSort = new JavaSortTest<>();
+        System.out.println(javaSort.sort(List.of("3", "2", "5", "4", "1")));
+
         System.out.println("Hello world!");
     }
 }
